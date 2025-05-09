e7b79ac0c1e6a3420d4839412aab55723f70b9fc
tsy85 <vitor.lazarini@gmail.com>
2025-05-09 10:32:03 -0400

    Task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,36 @@
 import requests
+import json
 def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     myobj = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    
     response = requests.post(url, json = myobj, headers=header)
-    return response.text
+    formatted_response = json.loads(response.text)
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+    dominant_emotion = "anger"
+    dominant_score = anger_score
+    if anger_score < disgust_score:
+        dominant_emotion = "disgust"
+        dominant_score = disgust_score
+    elif dominant_score < fear_score:
+        dominant_emotion = "fear"
+        dominant_score = fear_score
+    elif dominant_score < joy_score:
+        dominant_emotion = "joy"
+        dominant_score = joy_score
+    elif dominant_score < sadness_score:
+        dominant_emotion = "sadness"
+        dominant_score = sadness_score
+
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
